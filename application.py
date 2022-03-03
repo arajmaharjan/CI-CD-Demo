1417d211e8a6bceabed939ae1faeadd23a130c30
Ashish Maharjan <69330544+arajmaharjan@users.noreply.github.com>
2022-03-02 17:15:04 -0800

    Updated Input processing

--- application.py
+++ application.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 application = Flask(__name__)
-app = application
 
+@application.route('/', methods=['POST','GET'])
 
-@application.route('/')
-def hello_world():
-    return 'yeah you work!!.'
+def root_page():
+    name = ''
+    kg = ''  # weight
+    m = ''   # height
+    bmi = 0
+    if request.method == 'POST' and 'username' in request.form:
+        name = request.form.get('username')
+        kg = request.form.get('user_kg')
+        m = request.form.get('user_m')
+        bmi = round(float(kg)/float(m)**2)
+        # inputs are string, so convert to float
+    return render_template("index.html", name=name, kg=kg, m=m, bmi=bmi)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
